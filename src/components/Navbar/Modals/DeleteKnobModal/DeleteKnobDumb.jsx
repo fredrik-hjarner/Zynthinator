1575d3e2d5fb27c8aaf519d0a244c6b1a8f11bcd
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-01-11 22:08:50 +0100

    can delete several connections at once

--- src/components/Navbar/Modals/DeleteKnobModal/DeleteKnobDumb.jsx
+++ src/components/Navbar/Modals/DeleteKnobModal/DeleteKnobDumb.jsx
@@ -5,27 +5,28 @@ import {
 } from 'semantic-ui-react';
 import { BestModal } from '../commonImports';
 
-export const DeleteKnobDumb = (props) => {
-  const form = (
-    <Form inverted>
-      <Form.Field>
-        <label>knobs</label>
-        <Dropdown
-          onChange={props.onKnobIdChange}
-          options={props.options}
-          multiple
-          selection
-        />
-      </Form.Field>
-    </Form>);
+export const DeleteKnobDumb =
+  (props) => {
+    const form = (
+      <Form inverted>
+        <Form.Field>
+          <label>knobs</label>
+          <Dropdown
+            onChange={props.onKnobIdChange}
+            options={props.options}
+            multiple
+            selection
+          />
+        </Form.Field>
+      </Form>);
 
-  return (
-    <BestModal
-      {...props}
-      form={form}
-      header="Delete Knob"
-      description="Choose the knob to delete."
-      okButtonString="Delete it!"
-    />
-  );
-};
+    return (
+      <BestModal
+        {...props}
+        form={form}
+        header="Delete Knob"
+        description="Choose the knob to delete."
+        okButtonString="Delete it!"
+      />
+    );
+  };
