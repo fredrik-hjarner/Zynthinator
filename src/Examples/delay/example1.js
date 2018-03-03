4f8c5d855ee507287124ecb8a08de1709c93adec
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-03-03 18:33:47 +0100

    Repaired some examples.

--- src/Examples/delay/example1.js
+++ src/Examples/delay/example1.js
@@ -18,8 +18,23 @@ history =
       pathname: '/synth',
       search: '',
       hash: '',
-      key: 'ck479x'
+      key: 'upjyfe'
     }
+  },
+  {
+    type: 'OPEN_MODAL',
+    modal: 'CreateNodeModal',
+    props: {
+      nodeType: 'TimeDomainAnalyser'
+    }
+  },
+  {
+    type: 'CLOSE_MODAL'
+  },
+  {
+    type: 'CREATE_NODE',
+    name: 'delayed-sine',
+    nodeType: 'TimeDomainAnalyser'
   },
   {
     type: 'OPEN_MODAL',
@@ -48,27 +63,7 @@ history =
   },
   {
     type: 'CREATE_NODE',
-    name: 'sine1',
-    frequency: 440,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator'
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    name: 'sine2',
+    name: 'sine',
     frequency: 440,
     detune: 0,
     oscillatorType: 'sine',
@@ -83,414 +78,61 @@ history =
   {
     type: 'CLOSE_MODAL'
   },
-  // {
-  //   type: 'CREATE_CONNECTION',
-  //   name: '',
-  //   parentNodeIds: [
-  //     4
-  //   ],
-  //   childNodes: [
-  //     {
-  //       nodeId: 2,
-  //       input: 'input'
-  //     }
-  //   ]
-  // },
   {
-    type: 'OPEN_MODAL',
-    modal: 'CreateConnectionModal'
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  // {
-  //   type: 'CREATE_CONNECTION',
-  //   name: '',
-  //   parentNodeIds: [
-  //     5
-  //   ],
-  //   childNodes: [
-  //     {
-  //       nodeId: 3,
-  //       input: 'input'
-  //     }
-  //   ]
-  // },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'bitsToRecord',
-    value: '8'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'bitsToRecord',
-    value: '8'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'millisecondsBetweenUpdates',
-    value: '31'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'millisecondsBetweenUpdates',
-    value: '31'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator',
-      node: {
-        id: 5,
-        name: 'sine1',
-        frequency: 440,
-        detune: 0,
-        oscillatorType: 'sine',
-        minValue: -1,
-        maxValue: 1,
-        nodeType: 'Oscillator'
-      }
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    id: 5,
-    name: 'sine1',
-    frequency: 220,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'bitsToRecord',
-    value: '9'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator',
-      node: {
-        id: 5,
-        name: 'sine2',
-        frequency: 440,
-        detune: 0,
-        oscillatorType: 'sine',
-        minValue: -1,
-        maxValue: 1,
-        nodeType: 'Oscillator'
-      }
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    id: 5,
-    name: 'sine2',
-    frequency: 220,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'bitsToRecord',
-    value: '9'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'millisecondsBetweenUpdates',
-    value: '40'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'millisecondsBetweenUpdates',
-    value: '40'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator',
-      node: {
-        id: 5,
-        name: 'sine1',
-        frequency: 220,
-        detune: 0,
-        oscillatorType: 'sine',
-        minValue: -1,
-        maxValue: 1,
-        nodeType: 'Oscillator'
-      }
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    id: 5,
-    name: 'sine1',
-    frequency: 10,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'bitsToRecord',
-    value: '13'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'bitsToRecord',
-    value: '13'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator',
-      node: {
-        id: 6,
-        name: 'sine2',
-        frequency: 220,
-        detune: 0,
-        oscillatorType: 'sine',
-        minValue: -1,
-        maxValue: 1,
-        nodeType: 'Oscillator'
-      }
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    id: 6,
-    name: 'sine2',
-    frequency: 10,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'millisecondsBetweenUpdates',
-    value: '72'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator',
-      node: {
-        id: 5,
-        name: 'sine1',
-        frequency: 10,
-        detune: 0,
-        oscillatorType: 'sine',
-        minValue: -1,
-        maxValue: 1,
-        nodeType: 'Oscillator'
-      }
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    id: 5,
-    name: 'sine1',
-    frequency: 1,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'bitsToRecord',
-    value: '15'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 1,
-    param: 'millisecondsBetweenUpdates',
-    value: '40'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'bitsToRecord',
-    value: '15'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'Oscillator',
-      node: {
-        id: 6,
-        name: 'sine2',
-        frequency: 10,
-        detune: 0,
-        oscillatorType: 'sine',
-        minValue: -1,
-        maxValue: 1,
-        nodeType: 'Oscillator'
-      }
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
-    id: 6,
-    name: 'sine2',
-    frequency: 1,
-    detune: 0,
-    oscillatorType: 'sine',
-    minValue: -1,
-    maxValue: 1,
-    nodeType: 'Oscillator'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'PWM'
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'OPEN_MODAL',
-    modal: 'CreateNodeModal',
-    props: {
-      nodeType: 'TimeDomainAnalyser'
-    }
-  },
-  {
-    type: 'CLOSE_MODAL'
-  },
-  {
-    type: 'CREATE_NODE',
+    type: 'CREATE_CONNECTION',
     name: '',
-    nodeType: 'TimeDomainAnalyser'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 3,
-    param: 'bitsToRecord',
-    value: '15'
+    parentNodeIds: [
+      6
+    ],
+    childNodes: [
+      {
+        nodeId: 2,
+        input: 'input'
+      }
+    ]
   },
   {
     type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
     id: 1,
-    param: 'canvasWidth',
-    value: '280'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 2,
-    param: 'canvasWidth',
-    value: '280'
-  },
-  {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 3,
-    param: 'canvasWidth',
-    value: '280'
+    param: 'bitsToRecord',
+    value: '7'
   },
   {
     type: 'OPEN_MODAL',
-    modal: 'CreateConnectionModal'
+    modal: 'CreateNodeModal',
+    props: {
+      nodeType: 'Oscillator',
+      node: {
+        id: 6,
+        name: 'sine',
+        frequency: 440,
+        detune: 0,
+        oscillatorType: 'sine',
+        minValue: -1,
+        maxValue: 1,
+        nodeType: 'Oscillator'
+      }
+    }
   },
   {
     type: 'CLOSE_MODAL'
   },
-  // {
-  //   type: 'CREATE_CONNECTION',
-  //   name: '',
-  //   parentNodeIds: [
-  //     2,
-  //     3
-  //   ],
-  //   childNodes: [
-  //     {
-  //       nodeId: 6,
-  //       input: 'input'
-  //     }
-  //   ]
-  // },
   {
-    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 3,
-    param: 'maxValue',
-    value: '2'
+    type: 'CREATE_NODE',
+    id: 6,
+    name: 'sine',
+    frequency: 1,
+    detune: 0,
+    oscillatorType: 'sine',
+    minValue: -1,
+    maxValue: 1,
+    nodeType: 'Oscillator'
   },
   {
     type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
-    id: 3,
-    param: 'minValue',
-    value: '-2'
-  },
-  {
-    type: '@@INIT'
-  },
-  {
-    type: '@@router/LOCATION_CHANGE',
-    payload: {
-      pathname: '/synth',
-      search: '',
-      hash: '',
-      key: 'ck479x'
-    }
-  },
-  {
-    type: '@@INIT'
-  },
-  {
-    type: '@@router/LOCATION_CHANGE',
-    payload: {
-      pathname: '/synth',
-      search: '',
-      hash: '',
-      key: 'ck479x'
-    }
+    id: 1,
+    param: 'bitsToRecord',
+    value: '15'
   },
   {
     type: 'OPEN_MODAL',
@@ -510,42 +152,75 @@ history =
   },
   {
     type: 'OPEN_MODAL',
-    modal: 'InjectNodeModal'
+    modal: 'CreateConnectionModal'
   },
   {
     type: 'CLOSE_MODAL'
   },
   {
-    type: 'INJECT_NODE',
-    payload: {
-      nodeId: 7,
-      connectionId: 2
-    }
+    type: 'CREATE_CONNECTION',
+    name: '',
+    parentNodeIds: [
+      6
+    ],
+    childNodes: [
+      {
+        nodeId: 7,
+        input: 'input'
+      }
+    ]
   },
   {
     type: 'OPEN_MODAL',
-    modal: 'CreateKnobModal'
+    modal: 'CreateConnectionModal'
   },
   {
     type: 'CLOSE_MODAL'
   },
   {
-    type: 'CREATE_KNOB',
-    name: 'delay in seconds',
-    connectedToWhichNode: 7,
-    connectedToWhichParam: 'delayTime',
-    minValue: 0,
-    maxValue: 1,
-    function: 'linear'
+    type: 'CREATE_CONNECTION',
+    name: '',
+    parentNodeIds: [
+      7
+    ],
+    childNodes: [
+      {
+        nodeId: 4,
+        input: 'input'
+      }
+    ]
   },
-  // {
-  //   type: 'MOVE_KNOB',
-  //   id: 2,
-  //   value: 0.5
-  // },
-  // {
-  //   type: 'MOVE_KNOB',
-  //   id: 2,
-  //   value: 0
-  // }
+  {
+    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
+    id: 2,
+    param: 'bitsToRecord',
+    value: '15'
+  },
+  {
+    type: 'OPEN_MODAL',
+    modal: 'CreateConnectionModal'
+  },
+  {
+    type: 'CLOSE_MODAL'
+  },
+  {
+    type: 'CREATE_CONNECTION',
+    name: '',
+    parentNodeIds: [
+      2,
+      4
+    ],
+    childNodes: [
+      {
+        nodeId: 5,
+        input: 'input'
+      }
+    ]
+  },
+  {
+    type: 'UI_TD_ANALYSER_CHANGE_PARAMS',
+    id: 3,
+    param: 'bitsToRecord',
+    value: '15'
+  }
 ];
--- src/components/Navbar/menus/examples.js
+++ src/components/Navbar/menus/examples.js
@@ -11,10 +11,6 @@ export const Examples = () => (
       caption="1. Amplitude-modulated sine"
       onClick={E.example1}
     />
-    <MenuItem
-      caption="2. Simple music with low-res sine (must refresh page!!)"
-      onClick={E.example2}
-    />
     <MenuItem caption="Delay">
       <MenuItem caption="1. A sine and delayed sine summed" onClick={E.delay.example1} />
       <MenuItem caption="2. As nr. 1 but delay is sine-modulated" onClick={E.delay.example2} />
