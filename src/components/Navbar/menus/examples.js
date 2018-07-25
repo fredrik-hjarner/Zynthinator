4b8c93fd116afdee23e8cffebae1d4652652b350
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-07-25 19:34:30 +0200

    Extracting Examples to another project. WIP. Fixing eslint warnings.

--- src/components/Navbar/menus/examples.js
+++ src/components/Navbar/menus/examples.js
@@ -6,14 +6,25 @@ import {
 import * as E from 'Examples';
 import { importHistory } from 'redux/ReduxHistory';
 
+/**
+ * TODO: extract out MenuItem to something more specific that wraps the example json in a
+ * importHistory statement.
+ */
+
 const DigitalLogic = () => (
   <MenuItem caption="Digital logic">
     <MenuItem caption="SchmittTrigger">
-      <MenuItem caption="1. Knob input & thresholhold" onClick={() => importHistory(E.schmittTrigger.example1, 0)} />
+      <MenuItem
+        caption="1. Knob input & thresholhold"
+        onClick={() => importHistory(E.schmittTrigger.example1, 0)}
+      />
       <MenuItem caption="2. Knob sine gain" onClick={() => importHistory(E.schmittTrigger.example2, 0)} />
     </MenuItem>
     <MenuItem caption="TwoWaySwitch">
-      <MenuItem caption="1. Switching between triangle and sine" onClick={() => importHistory(E.twoWaySwitch.example1, 0)} />
+      <MenuItem
+        caption="1. Switching between triangle and sine"
+        onClick={() => importHistory(E.twoWaySwitch.example1, 0)}
+      />
     </MenuItem>
   </MenuItem>
 );
@@ -22,7 +33,10 @@ const Distortions = () => (
   <MenuItem caption="Distortions">
     <MenuItem caption="CrossoverDistortion">
       <MenuItem caption="1. Basic example" onClick={() => importHistory(E.crossoverDistortion.example1, 0)} />
-      <MenuItem caption="1. Square enables/disables distortion" onClick={() => importHistory(E.crossoverDistortion.example2, 0)} />
+      <MenuItem
+        caption="1. Square enables/disables distortion"
+        onClick={() => importHistory(E.crossoverDistortion.example2, 0)}
+      />
     </MenuItem>
   </MenuItem>
 );
@@ -61,13 +75,25 @@ const Oscillators = () => (
 const Quantizers = () => (
   <MenuItem caption="Quantizers">
     <MenuItem caption="Quantizer">
-      <MenuItem caption="1. Change resolution of sine with knob" onClick={() => importHistory(E.quantizer.example1, 0)} />
-      <MenuItem caption="2. Triangle changing resolution of sine" onClick={() => importHistory(E.quantizer.example2, 0)} />
+      <MenuItem
+        caption="1. Change resolution of sine with knob"
+        onClick={() => importHistory(E.quantizer.example1, 0)}
+      />
+      <MenuItem
+        caption="2. Triangle changing resolution of sine"
+        onClick={() => importHistory(E.quantizer.example2, 0)}
+      />
     </MenuItem>
     <MenuItem caption="TimeQuantizer">
       <MenuItem caption="1. Quantized noise" onClick={() => importHistory(E.timeQuantizer.example1, 0)} />
-      <MenuItem caption="2. Playing random tones" onClick={() => importHistory(E.timeQuantizer.example2, 0)} />
-      <MenuItem caption="3. Like 2. but sine modulates speed of melody" onClick={() => importHistory(E.timeQuantizer.example3, 0)} />
+      <MenuItem
+        caption="2. Playing random tones"
+        onClick={() => importHistory(E.timeQuantizer.example2, 0)}
+      />
+      <MenuItem
+        caption="3. Like 2. but sine modulates speed of melody"
+        onClick={() => importHistory(E.timeQuantizer.example3, 0)}
+      />
     </MenuItem>
   </MenuItem>
 );
@@ -82,8 +108,14 @@ export const Examples = () => (
       <MenuItem caption="1. Knob max val of sine" onClick={() => importHistory(E.changeRange.example1, 0)} />
     </MenuItem>
     <MenuItem caption="Delay">
-      <MenuItem caption="1. A sine and delayed sine summed" onClick={() => importHistory(E.delay.example1, 0)} />
-      <MenuItem caption="2. As nr. 1 but delay is sine-modulated" onClick={() => importHistory(E.delay.example2, 0)} />
+      <MenuItem
+        caption="1. A sine and delayed sine summed"
+        onClick={() => importHistory(E.delay.example1, 0)}
+      />
+      <MenuItem
+        caption="2. As nr. 1 but delay is sine-modulated"
+        onClick={() => importHistory(E.delay.example2, 0)}
+      />
     </MenuItem>
     <DigitalLogic/>
     <Distortions/>
